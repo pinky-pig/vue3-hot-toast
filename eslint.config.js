bbf93bcefa8efd1bdcbedc9f67f4d44bcd2c0d15
pinky-pig <2545589993@qq.com>
2024-06-05 01:16:16 +0800

    chore: 添加 eslint 和 prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,26 +2,42 @@
 /* eslint perfectionist/sort-objects: "error" */
 // import antfu from '@antfu/eslint-config'
 
-const antfu = require('@antfu/eslint-config').default
+import { arvinn } from '@arvinn/eslint-config'
 
-module.exports = antfu(
-  {
-    formatters: true,
-  },
-  {
-    rules: {
-      'no-labels': 'off',
-      'no-lone-blocks': 'off',
-      'no-restricted-syntax': 'off',
-      'node/prefer-global/buffer': 'off',
-      'node/prefer-global/process': 'off',
-      'prefer-rest-params': 'off',
-      'symbol-description': 'off',
-      'ts/ban-types': 'off',
-      'ts/no-invalid-this': 'off',
-      'ts/no-unnecessary-type-constraint': 'off',
-      'vue/no-template-shadow': 'off',
-      'vue/no-v-text-v-html-on-component': 'off',
+export default arvinn(
+  [
+    {
+      rules: {
+        'no-labels': 'off',
+        'no-lone-blocks': 'off',
+        'no-restricted-syntax': 'off',
+        'node/prefer-global/buffer': 'off',
+        'node/prefer-global/process': 'off',
+        'prefer-rest-params': 'off',
+        'symbol-description': 'off',
+        'ts/ban-types': 'off',
+        'ts/no-invalid-this': 'off',
+        'ts/no-unnecessary-type-constraint': 'off',
+        'vue/no-template-shadow': 'off',
+        'vue/no-v-text-v-html-on-component': 'off',
+      },
     },
+    {
+      files: ['src/**/*.ts'],
+      rules: {
+        'perfectionist/sort-objects': 'error',
+      },
+    },
+    {
+      files: ['**/*.md/*'],
+      rules: {
+        'sort-imports': 'off',
+      },
+    },
+  ],
+
+  {
+    prettier: true,
+    vue: true,
   },
 )
